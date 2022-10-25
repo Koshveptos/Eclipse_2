b106263497385a2047c06aab02cca7410a6b7fb2
koshveptos <vas.cazakow2016@yandex.ru>
2022-10-25 17:56:15 +0300

    В абитуриентах сделал правильные ссылки

--- eclipse/eclipse/settings.py
+++ eclipse/eclipse/settings.py
@@ -120,8 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'abityra/static'),
+    os.path.join(BASE_DIR, 'attractions/static'),
+    os.path.join(BASE_DIR, 'history/static'),
+    os.path.join(BASE_DIR, 'it/static'),
+    os.path.join(BASE_DIR, 'tagasite/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
