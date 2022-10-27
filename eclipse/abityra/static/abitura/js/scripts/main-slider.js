d8b3caf3a0d251fd77195496fcdc97a17d6e558d
Kpyr <igrovaaplaneta@gmail.com>
2022-10-27 03:58:12 +0300

    UPD: - Fix button - Fix message

--- eclipse/abityra/static/abitura/js/scripts/main-slider.js
+++ eclipse/abityra/static/abitura/js/scripts/main-slider.js
@@ -1,8 +1,9 @@
 function poliToLeft(){
     pedToRight();
+
     document.getElementById("POLI-main-block").style.animationDuration = "1.5s";
     document.getElementById("POLI-main-block").style.animationName = "PoliToLeft";
-    document.getElementById("POLI-main-block").style.left = "182px";
+    document.querySelector('#POLI-main-block').style.left = "43rem";
 
     document.getElementById("SFEDU-bg-1").style.display = "none";
     document.getElementById("SFEDU-bg-2").style.display = "block";
@@ -15,18 +16,20 @@ function poliToLeft(){
 }
 
 function poliToRight(){
+    if(document.getElementById("POLI-main-block").style.left === "43rem"){
+        document.getElementById("POLI-main-block").style.animationDuration = "1.5s";
+        document.getElementById("POLI-main-block").style.animationName = "PoliToRight";
+        document.getElementById("POLI-main-block").style.left = "130rem";
+        document.getElementById("SFEDU-bg-1").style.display = "block";
+        document.getElementById("SFEDU-bg-2").style.display = "none";
+        document.getElementById("SFEDU-bg-3").style.display = "none";
+
+
+        document.getElementById("SFEDU").style.display = "block";
+        document.getElementById("POLI").style.display = "none";
+        document.getElementById("PED").style.display = "none";
+    }
     pedToRight();
-    document.getElementById("POLI-main-block").style.animationDuration = "1.5s";
-    document.getElementById("POLI-main-block").style.animationName = "PoliToRight";
-    document.getElementById("POLI-main-block").style.left = "1097px";
-    document.getElementById("SFEDU-bg-1").style.display = "block";
-    document.getElementById("SFEDU-bg-2").style.display = "none";
-    document.getElementById("SFEDU-bg-3").style.display = "none";
-
-
-    document.getElementById("SFEDU").style.display = "block";
-    document.getElementById("POLI").style.display = "none";
-    document.getElementById("PED").style.display = "none";
 }
 
 
@@ -35,7 +38,7 @@ function pedToLeft(){
     poliToLeft();
     document.getElementById("PED-main-block").style.animationDuration = "1.5s";
     document.getElementById("PED-main-block").style.animationName = "PedToLeft";
-    document.getElementById("PED-main-block").style.left = "364px";
+    document.getElementById("PED-main-block").style.left = "61rem";
     document.getElementById("SFEDU-bg-1").style.display = "none";
     document.getElementById("SFEDU-bg-2").style.display = "none";
     document.getElementById("SFEDU-bg-3").style.display = "block";
@@ -46,7 +49,9 @@ function pedToLeft(){
 }
 
 function pedToRight(){
-    document.getElementById("PED-main-block").style.animationDuration = "1.5s";
-    document.getElementById("PED-main-block").style.animationName = "PedToRight";
-    document.getElementById("PED-main-block").style.left = "1279px";
+    if(document.getElementById("PED-main-block").style.left === "61rem"){
+        document.getElementById("PED-main-block").style.animationDuration = "1.5s";
+        document.getElementById("PED-main-block").style.animationName = "PedToRight";
+        document.getElementById("PED-main-block").style.left = "148rem";
+    }
 }
--- eclipse/abityra/static/abitura/js/scripts/second-slider.js
+++ eclipse/abityra/static/abitura/js/scripts/second-slider.js
@@ -1,3 +1,106 @@
+
+const screenWidth = window.screen.width
+
+if(screenWidth >= 600){
+    let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+        li.style.position = 'relative';
+        li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+        i++;
+    }
+
+    /* конфигурация */
+    var l = document.getElementById("fix");
+
+    var element = document.getElementById('fix');
+    var style = window.getComputedStyle(element, null);
+    var width1 = element.offsetWidth;
+    var margin = parseInt(style.marginRight);
+
+
+
+    let width = (l.offsetWidth + margin)/10;
+
+
+    /*let width = document.getElementById("fix").offsetWidth;*/
+    /*let width = 33 + 10; // ширина картинки*/
+    let count = 4; // видимое количество изображений
+
+
+
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // положение ленты прокрутки
+
+    carousel.querySelector('.button-prev').onclick = function() {
+        // сдвиг влево
+        position += width * 1;
+        // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+        position = Math.min(position, 0)
+        list.style.marginLeft = position + 'rem';
+    };
+
+    carousel.querySelector('.button-next').onclick = function() {
+        // сдвиг вправо
+        position -= width;
+        position = Math.max(position, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'rem';
+    };
+}
+else{
+    let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+        li.style.position = 'relative';
+        li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+        i++;
+    }
+
+    /* конфигурация */
+    var l = document.getElementById("fix");
+
+    /*var element = document.getElementById('fix');
+    var style = window.getComputedStyle(element, null)
+    var width1 = element.offsetWidth;
+    var margin = parseInt(style.marginRight);*/
+
+
+
+    /*let width = (l.offsetWidth + margin)/10;*/
+    let width = (l.offsetWidth + 7)/1.9;
+
+
+    /*let width = document.getElementById("fix").offsetWidth;*/
+    /*let width = 33 + 10; // ширина картинки*/
+    let count = 3; // видимое количество изображений
+
+
+
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // положение ленты прокрутки
+
+    carousel.querySelector('.button-prev').onclick = function() {
+        // сдвиг влево
+        position += width * 1;
+        // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+        position = Math.min(position, 0)
+        list.style.marginLeft = position + 'rem';
+    };
+
+    carousel.querySelector('.button-next').onclick = function() {
+        // сдвиг вправо
+        position -= width;
+        position = Math.max(position, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'rem';
+    };
+}
+
+
+
 function disabledall(){
     for (let i = 0; i < 11; i++) {
         document.getElementsByClassName("cafedra")[i].style.display = "none";
@@ -63,39 +166,7 @@ function cf11(){
 
 
 
-let i = 1;
-for(let li of carousel.querySelectorAll('li')) {
-    li.style.position = 'relative';
-    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-    i++;
-}
 
-/* конфигурация */
-let width = 330 + 53 + 53; // ширина картинки
-let count = 4; // видимое количество изображений
-
-let list = carousel.querySelector('ul');
-let listElems = carousel.querySelectorAll('li');
-
-let position = 0; // положение ленты прокрутки
-
-carousel.querySelector('.button-prev').onclick = function() {
-    // сдвиг влево
-    position += width * 1;
-    // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-    position = Math.min(position, 0)
-    list.style.marginLeft = position + 'px';
-};
-
-carousel.querySelector('.button-next').onclick = function() {
-    // сдвиг вправо
-    position -= width * 1;
-    /*position -= width / 2;*/
-    /*position -= width / 2;*/
-    // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-    position = Math.max(position, -width * (listElems.length - count));
-    list.style.marginLeft = position + 'px';
-};
 
 
 function poliToLeft(){
@@ -105,9 +176,6 @@ function poliToLeft(){
     document.getElementById("POLI-main-block").style.left = "182px";
 
 
-    /*document.getElementById("SFEDU-bg-1").style.width = "182px";
-    document.getElementById("SFEDU-bg-1").style.height = "711px";
-    document.getElementById("SFEDU-bg-1").style.borderRadius = "20px 0px 0px 20px";*/
 
     document.getElementById("SFEDU-bg-1").style.display = "none";
     document.getElementById("SFEDU-bg-2").style.display = "block";
